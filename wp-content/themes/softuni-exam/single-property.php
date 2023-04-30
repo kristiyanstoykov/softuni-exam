8e1809958c0fd06f4f6ec04b142e55412247d075
kristian <krisistoikov@gmail.com>
2023-04-30 13:59:39 +0300

    Added single template for homes

---
+++ wp-content/themes/softuni-exam/single-property.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<?php if ( have_posts() ) : ?>
+
+    <ul class="properties-listing">
+
+    <?php while ( have_posts() ) : ?>
+        <?php the_post(); ?>
+        
+        <?php get_template_part( 'template-parts/single', 'property' ); ?>
+
+    <?php endwhile ?>
+
+    </ul>
+        
+    <?php endif ?>
+		
+<?php get_footer(); ?>
