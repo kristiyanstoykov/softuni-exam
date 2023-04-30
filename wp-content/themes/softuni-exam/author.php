efa8b8ad289fbd33f8949f7fc448135a4696df4d
kristian <krisistoikov@gmail.com>
2023-04-30 11:11:04 +0300

    Added author post template

---
+++ wp-content/themes/softuni-exam/author.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+
+<h1><?php the_archive_title(); ?></h1>
+
+<?php //if ( !empty( the_author_meta( 'user_description' ) ) ) : ?>
+    <div>
+        <p><?php esc_html_e( the_author_meta( 'user_description' ) ); ?></p>
+    </div>
+<?php //endif ?>
+
+<?php if ( have_posts() ) : ?>
+
+    <ul class="jobs-listing">
+        <?php while ( have_posts() ) : ?>
+
+            <?php the_post(); ?>
+
+            <?php get_template_part( '/template-parts/property', 'item' ); ?>
+
+        <?php endwhile ?>
+    </ul>
+<?php else : ?>
+
+    <h3><?php esc_html_e( 'No posts found' )?></h3>
+
+<?php endif ?>
+
+
+<?php get_footer(); ?>
