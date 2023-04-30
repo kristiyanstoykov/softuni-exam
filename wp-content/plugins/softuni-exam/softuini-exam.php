1a9dd21527fd93539e1b9f63fd320fe102327545
kristian <krisistoikov@gmail.com>
2023-04-30 11:10:33 +0300

    Added header for plugin and requering the cpt property

---
+++ wp-content/plugins/softuni-exam/softuini-exam.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name:       Softuni course exam plugin
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Custom plugin that manages the properties
+ * Version:           0.1
+ * Requires at least: 5
+ * Requires PHP:      7.2
+ * Author:            Kristiyan Stoykov
+ * Author URI:        https://example.com
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       softuni-homes
+ */
+
+
+/**
+ * Load our custom post types
+ */
+require 'cpt-property.php';
