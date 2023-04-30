c8117e58fd24d0d2538de4e7e166fae44b55cea7
kristian <krisistoikov@gmail.com>
2023-04-30 11:11:15 +0300

    Added post item template

---
+++ wp-content/themes/softuni-exam/template-parts/post-item.php
@@ -0,0 +1,29 @@
+<li class="property-card">
+    <div class="property-primary">
+        <h2 class="property-title">
+            <a href="<?php esc_html_e( get_permalink()); ?>">
+                <?php esc_html_e( get_the_title() ); ?>
+            </a>
+        </h2>
+        <div class="property-meta">
+            <span class="categories">
+                <?php the_category(); ?>
+            </span>
+        </div>
+        <div class="property-details">
+            <span class="property-date"> <?php esc_html_e( "Posted on " . get_the_date( 'd-m-Y' ), "softuni-homes"  ); ?></span>
+        </div>
+    </div>
+    <div class="property-image">
+        <div class="property-image-box">
+            <?php
+                if ( has_post_thumbnail() ) {
+                    the_post_thumbnail();
+                }
+                // else {
+                //     esc_html_e( '<img src="' . get_template_directory() . '/images/bedroom.jpg">' );
+                // }
+            ?>
+        </div>
+    </div>
+</li>
