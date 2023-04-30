64c922ee50b1c1e022f89c6afc5763c48aadcfe7
kristian <krisistoikov@gmail.com>
2023-04-30 10:03:43 +0300

    Fixed typos

---
+++ wp-content/themes/softuni-exam/footer.php
@@ -0,0 +1,14 @@
+		
+		<footer class="site-footer">
+			<p>&copy; Copyright <?php esc_html_e( date( 'Y' ) ); ?> | Developer links: 
+				<a href="#">Edits</a>,
+				<a href="#">Home</a>,
+				<a href="#">Single</a>
+			</p>
+		</footer>
+	</div>
+
+    <?php wp_footer(); ?>
+
+</body>
+</html> 
