43b9e620fd33cda71c5570df2b3858844ed19f01
kristian <krisistoikov@gmail.com>
2023-04-30 12:20:55 +0300

    Added post navigation

--- wp-content/themes/softuni-exam/author.php
+++ wp-content/themes/softuni-exam/author.php
@@ -1,29 +1,32 @@
 <?php get_header(); ?>
 
-<h1><?php the_archive_title(); ?></h1>
+    <h1><?php the_archive_title(); ?></h1>
 
-<?php //if ( !empty( the_author_meta( 'user_description' ) ) ) : ?>
-    <div>
-        <p><?php esc_html_e( the_author_meta( 'user_description' ) ); ?></p>
-    </div>
-<?php //endif ?>
+    <?php //if ( !empty( the_author_meta( 'user_description' ) ) ) : ?>
+        <div>
+            <p><?php esc_html_e( the_author_meta( 'user_description' ) ); ?></p>
+        </div>
+    <?php //endif ?>
 
-<?php if ( have_posts() ) : ?>
+    <?php if ( have_posts() ) : ?>
 
-    <ul class="jobs-listing">
-        <?php while ( have_posts() ) : ?>
+        <ul class="jobs-listing">
+            <?php while ( have_posts() ) : ?>
 
-            <?php the_post(); ?>
+                <?php the_post(); ?>
 
-            <?php get_template_part( '/template-parts/property', 'item' ); ?>
+                <?php get_template_part( '/template-parts/property', 'item' ); ?>
 
-        <?php endwhile ?>
-    </ul>
-<?php else : ?>
+            <?php endwhile ?>
+        </ul>
 
-    <h3><?php esc_html_e( 'No posts found' )?></h3>
+        <?php echo posts_nav_link(); ?>
+        
+    <?php else : ?>
 
-<?php endif ?>
+        <h3><?php esc_html_e( 'No posts found' )?></h3>
+
+    <?php endif ?>
 
 
 <?php get_footer(); ?>
